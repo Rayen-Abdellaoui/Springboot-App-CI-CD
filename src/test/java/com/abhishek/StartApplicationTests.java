dce572cd80b6d9bfb5be4431b7e4f8d54f6c01cf
Rayen Abdellaoui <abdellaouirayen219@gmail.com>
2025-07-31 13:04:17 +0100

    add more tests

--- src/test/java/com/abhishek/StartApplicationTests.java
+++ src/test/java/com/abhishek/StartApplicationTests.java
@@ -1,9 +1,9 @@
 package com.abhishek;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,11 +17,16 @@ class StartApplicationTests {
     private MockMvc mockMvc;
 
     @Test
-    void testIndexPage() throws Exception {
+    void testIndexEndpoint() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("title"))
-                .andExpect(model().attributeExists("msg"))
-                .andExpect(view().name("index"));
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("title", "This is a SpringBoot Static Web Application"))
+                .andExpect(model().attribute("msg", "Application Is Deployed To Kuberneets"));
+    }
+
+    @Test
+    void testMainMethodRuns() {
+        StartApplication.main(new String[]{});
     }
 }
