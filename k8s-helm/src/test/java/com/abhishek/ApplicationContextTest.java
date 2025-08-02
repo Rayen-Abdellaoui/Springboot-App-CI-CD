5f69a86aa1815720a0de51937b42288c7a177e41
Rayen Abdellaoui <abdellaouirayen219@gmail.com>
2025-08-02 15:43:32 +0100

    Create SpringbootPodCrashloop Rule

---
+++ k8s-helm/src/test/java/com/abhishek/ApplicationContextTest.java
@@ -0,0 +1,12 @@
+package com.abhishek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ k8s-helm/src/test/java/com/abhishek/StartApplicationTests.java
@@ -0,0 +1,32 @@
+package com.abhishek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class StartApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testIndexEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("title", "This is a SpringBoot Static Web Application"))
+                .andExpect(model().attribute("msg", "Application Is Deployed To Kuberneets"));
+    }
+
+    @Test
+    void testMainMethodRuns() {
+        StartApplication.main(new String[]{});
+    }
+}
